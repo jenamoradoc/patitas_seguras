44b73e94164438a250d91b00a12215a2f04050fd
David Enamorado <davidenamorado@Davids-MacBook-Pro.local>
2024-01-26 18:13:45 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/patitas_seguras/',
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
